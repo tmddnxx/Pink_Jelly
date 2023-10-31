35f37cfa47407efb9960b1ec554b57d47054e41b
LeeHeonGu <gjsrn2006@naver.com>
2023-10-31 17:25:54 +0900

    Add Mapper

---
+++ src/main/java/com/example/pink_jelly/mapper/CRBCommentMapper.java
@@ -0,0 +1,8 @@
+package com.example.pink_jelly.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper // mybatis 사용할 때는 선언해줘야함
+public interface CRBCommentMapper {
+
+}
---
+++ src/main/java/com/example/pink_jelly/mapper/CatsMeMapper.java
@@ -0,0 +1,10 @@
+package com.example.pink_jelly.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper // mybatis 사용할 때는 선언해줘야함
+public interface CatsMeMapper {
+
+}
