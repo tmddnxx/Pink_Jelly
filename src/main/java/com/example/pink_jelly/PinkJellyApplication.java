694d721706600aa133ca3e0f05c4cc538e94f600
LeeHeonGu <gjsrn2006@naver.com>
2023-10-31 12:42:36 +0900

    First Pink_Jelly

---
+++ src/main/java/com/example/pink_jelly/PinkJellyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pink_jelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkJellyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkJellyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pink_jelly/PinkJellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pink_jelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkJellyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
