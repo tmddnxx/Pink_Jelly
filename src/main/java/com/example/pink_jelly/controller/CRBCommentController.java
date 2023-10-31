bd1cc70d4bff23a69099ed8a0d4cb430d902e14f
LeeHeonGu <gjsrn2006@naver.com>
2023-10-31 17:18:23 +0900

    Add Mapping

--- src/main/java/com/example/pink_jelly/controller/CRBCommentController.java
+++ src/main/java/com/example/pink_jelly/controller/CRBCommentController.java
@@ -1,4 +1,13 @@
 package com.example.pink_jelly.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/crbComment")
+@RequiredArgsConstructor
 public class CRBCommentController {
 }
--- src/main/java/com/example/pink_jelly/controller/LocationController.java
+++ src/main/java/com/example/pink_jelly/controller/LocationController.java
@@ -1,4 +1,13 @@
 package com.example.pink_jelly.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequestMapping("/location")
+@RequiredArgsConstructor
 public class LocationController {
 }
