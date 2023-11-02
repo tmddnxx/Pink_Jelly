67af90e2be75a5b6db58d952cd3cb2f4b6d741fd
LeeHeonGu <gjsrn2006@naver.com>
2023-11-02 15:06:13 +0900

    Add UI

--- src/main/java/com/example/pink_jelly/controller/MainBoardController.java
+++ src/main/java/com/example/pink_jelly/controller/MainBoardController.java
@@ -3,6 +3,7 @@ package com.example.pink_jelly.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
@@ -10,5 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 @RequiredArgsConstructor
 public class MainBoardController {
-
+    @GetMapping("")
+    public String main() {
+        System.out.println("main GET...");
+        return "main/index";
+    }
 }
