b35545469ba02e8e6bf321026b76ff3ecd15ce9f
LeeHeonGu <gjsrn2006@naver.com>
2023-11-30 13:51:46 +0900

    이헌구 에러 수정

--- src/main/java/com/example/pink_jelly/config/CustomSecurityConfig.java
+++ src/main/java/com/example/pink_jelly/config/CustomSecurityConfig.java
@@ -114,7 +114,7 @@ public class CustomSecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/", "/member/signup", "/member/welcome", "/member/sendConfirmMail",
-                        "/member/matchConfirmKey", "/member/removeConfirmKey", "/member/checkMemberId").permitAll()
+                        "/member/matchConfirmKey", "/member/removeConfirmKey", "/member/checkMemberId", "/member/exit").permitAll()
                 .antMatchers("/member/**", "/profile/myProfile", "/main/write", "/main/modify", "/catsMe/board/write", "/catsMe/board/modify",
                         "/catsMe/review/write", "/catsMe/review/modify", "/chat/**").authenticated()
                 .anyRequest().permitAll();
